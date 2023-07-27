4072359689fe6f398e6ba5a2f944556bffae8b8c
eden1207 <alexis.mauray@gmail.com>
2023-07-27 16:28:11 +0200

    Ajout du front-end/ avec les bugs résolus

---
+++ Billed-app-FR-Front/src/views/BillsUI.js
@@ -0,0 +1,104 @@
+import VerticalLayout from './VerticalLayout.js'
+import ErrorPage from "./ErrorPage.js"
+import LoadingPage from "./LoadingPage.js"
+
+import Actions from './Actions.js'
+
+const row = (bill) => {
+  return (`
+    <tr>
+      <td>${bill.type}</td>
+      <td>${bill.name}</td>
+      <td>${bill.date}</td>
+      <td>${bill.amount} €</td>
+      <td>${bill.status}</td>
+      <td>
+        ${Actions(bill.fileUrl)}
+      </td>
+    </tr>
+    `)
+  }
+
+const rows = (data) => {
+
+  /**
+   * I have deleted the following line code ...
+   * return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+   * And I droped this  package, sorting not formatted dates and returning
+   * none formatted dates (if you want to, you need to <td>${bill.date}</td> l.12 by 
+   * <td>${bill.rowDate}</td>)
+   * This change is important because the Bills.js tests dates with the format looking like '2001-02-16'
+   */
+  if(data && data.length) {
+    const dates = [...data].sort((a, b) => {
+      const elementA = new Date(a.date).getTime();
+      const elementB = new Date(b.date).getTime();
+      if (elementA < elementB) {
+        return 1;
+      }
+      if (elementA > elementB) {
+        return -1;
+      }
+      return 0;
+    });
+    return dates.map(bill => row(bill)).join("")
+  } else{
+    return ""
+  }
+}
+
+export default ({ data: bills, loading, error }) => {
+  
+  const modal = () => (`
+    <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLongTitle">Justificatif</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+          </div>
+        </div>
+      </div>
+    </div>
+  `)
+
+  if (loading) {
+    return LoadingPage()
+  } else if (error) {
+    return ErrorPage(error)
+  }
+  
+  return (`
+    <div class='layout'>
+      ${VerticalLayout(120)}
+      <div class='content'>
+        <div class='content-header'>
+          <div class='content-title'> Mes notes de frais </div>
+          <button type="button" data-testid='btn-new-bill' class="btn btn-primary">Nouvelle note de frais</button>
+        </div>
+        <div id="data-table">
+          <table id="example" class="table table-striped" style="width:100%">
+            <thead>
+              <tr>
+                <th>Type</th>
+                <th>Nom</th>
+                <th>Date</th>
+                <th>Montant</th>
+                <th>Statut</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody data-testid="tbody">
+              ${rows(bills)}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      ${modal()}
+    </div>`
+  )
+}
